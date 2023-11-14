08969a9bd36984bcebeb023bc80935fad6f6e501
turulomio <turulomio@yahoo.es>
2023-11-14 13:47:57 +0100

    Añadido primer test getInteger

--- django_test/testme/views.py
+++ django_test/testme/views.py
@@ -1,9 +1,7 @@
-
 from request_casting import request_casting
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import  status
-
 
 @api_view(['GET', 'POST'])    
 def integer(request):
