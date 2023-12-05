705094b1f69eb5bd5b542053693b0440ffe33a1f
turulomio <turulomio@yahoo.es>
2023-12-05 03:24:41 +0100

    request_casting-0.4.0

--- request_casting/__init__.py
+++ request_casting/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="0.3.0"
-__version_datetime__=datetime(2023, 12, 4, 12, 50)
+__version__="0.4.0"
+__version_datetime__=datetime(2023, 12, 5, 3, 22)
 __version_date__=__version_datetime__.date()
