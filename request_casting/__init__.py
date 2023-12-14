4b4b3caf233370a126afd8c75ac7723d747ba644
turulomio <turulomio@yahoo.es>
2023-12-14 20:41:35 +0100

    request_casting-0.6.0 (#20)
    * poetry update
    
    * poe coverage is 96%
    
    * request_casting-0.6.0

--- request_casting/__init__.py
+++ request_casting/__init__.py
@@ -1,4 +1,4 @@
 from datetime import datetime
-__version__="0.5.0"
-__version_datetime__=datetime(2023, 12, 5, 5, 33)
+__version__="0.6.0"
+__version_datetime__=datetime(2023, 12, 14, 20, 39)
 __version_date__=__version_datetime__.date()
