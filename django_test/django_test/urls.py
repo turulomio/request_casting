be8664b7b4b42dcaa22cfd0c0eeae475fff16758
turulomio <turulomio@yahoo.es>
2024-04-13 07:29:19 +0200

    Fixed error in urls

--- django_test/django_test/urls.py
+++ django_test/django_test/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 
 router = routers.DefaultRouter()
 router.register(r'records', views.RecordViewSet)
-router.register(r'posts', views.RecordViewSet)
+router.register(r'posts', views.PostsViewSet)
 
 urlpatterns = [
     path('api/', include(router.urls)),
